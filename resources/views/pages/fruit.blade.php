be7f1c8abad2370bf0abe56692d20cdfd6b267ae
saarahchairi <sarni555@hotmail.com>
2022-08-08 11:43:33 +0200

    fruit page done

--- resources/views/pages/fruit.blade.php
+++ resources/views/pages/fruit.blade.php
@@ -1,5 +1,10 @@
 @extends('layout.index')
 
 @section('content')
+    <?php
+    $fruit = ['pomme', 'banane', 'cerise', 'fraise'];
 
+    for($i = 0; $i < count($fruit); $i++)
+        echo $fruit[$i]."\n";
+    ?>
 @endsection
